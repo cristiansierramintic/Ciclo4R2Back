8aa47db09988643def18e2f533b948de65b2dc5f
cristiansierramintic <91471948+cristiansierramintic@users.noreply.github.com>
2021-12-10 18:31:42 -0500

    first commit

---
+++ src/main/java/com/ciclo4/reto2/model/Product.java
@@ -0,0 +1,27 @@
+package com.ciclo4.reto2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "product")
+public class Product {
+    
+    @Id
+    private Integer id;
+    private String brand;
+    private String procesor;
+    private String os;
+    private String description;
+    private String memory;
+    private String hardDrive;
+    private boolean availability = true;
+    private double price;
+    private int quantity;
+    private String photography;
+}
---
+++ src/main/java/com/ciclo4/reto2/repository/crud/ProductCrudRepository.java
@@ -0,0 +1,8 @@
+package com.ciclo4.reto2.repository.crud;
+
+import com.ciclo4.reto2.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductCrudRepository extends MongoRepository<Product, Integer>{
+    
+}
